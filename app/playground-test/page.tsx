20e6c4142d8d6d14fe592987305a9baa2031c11b
samaaali-09 <semaaali76@gmail.com>
2026-08-05 03:02:40 +0300

    feat: add accessible Tabs component with arrow key navigation

--- app/playground-test/page.tsx
+++ app/playground-test/page.tsx
@@ -1,7 +1,14 @@
 'use client';
 
 import ModalDemo from '../../playground/demo';
+import TabsDemo from '../../playground/tabs-demo';
 
 export default function PlaygroundTestPage() {
-  return <ModalDemo />;
+  return (
+    <div>
+      <ModalDemo />
+      <hr style={{ margin: '40px 0' }} />
+      <TabsDemo />
+    </div>
+  );
 }
